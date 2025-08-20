2fbfea690a689ecafb649a79af60f044e52ab657
sijin98 <164668407+sijin98@users.noreply.github.com>
2025-08-20 12:11:31 +0530

    watchlist functionality added

--- src/App.js
+++ src/App.js
@@ -1,37 +1,75 @@
-import './App.css';
-import './styles.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
-import WatchList from './components/WatchList';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import WatchList from "./components/WatchList";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { React, useEffect, useState } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const toggleWatchlist = (movieId) => {
+    setWatchlist((prev) =>
+      prev.includes(movieId)
+        ? prev.filter((id) => id !== movieId)
+        : [...prev, movieId]
+    );
+  };
+
   return (
     <div className="App">
       <header className="header">
-       <h1 style={{color:"red"}}> Welcome to Moviedux</h1>
+        <h1 style={{ color: "white" }}> Welcome to Moviedux</h1>
       </header>
       <div className="container">
-      <Header/>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-              <Link to="/watchlist">Watch List</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/' element={<MoviesGrid/>}></Route>
-          <Route path='/watchlist' element={<WatchList/>}></Route>
-        </Routes>
-      </Router>
+        <Header />
+        <Router>
+          <nav className="nav">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watch List</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MoviesGrid
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchlist={toggleWatchlist}
+                />
+              }
+            ></Route>
+            <Route
+              path="/watchlist"
+              element={
+                <WatchList
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchlist={toggleWatchlist}
+                />
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </div>
 
       <footer className="footer">
-        <Footer/>
+        <Footer />
       </footer>
     </div>
   );
--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,10 +1,23 @@
-import React from "react";
 import "../styles.css";
+import MovieCard from "./MovieCard";
 
-export default function WatchList(){
-    return(
-        <div>
-            <h1>Watch List</h1>
-        </div>
-    );
+export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
+  return (
+    <div>
+      <h1 className="title">Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <MovieCard
+              key={id}
+              movie={movie}
+              toggleWatchlist={toggleWatchlist}
+              isWatchlisted={true}
+            ></MovieCard>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
